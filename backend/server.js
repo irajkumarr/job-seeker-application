f477c4dcbcd5c5485acb6ef70f5dcdb6fffd4e1f
Raj Kumar Timalsina <rajkumar.pc009@gmail.com>
2025-01-15 22:35:34 +0545

    connection to mongo db and server

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+const cors = require("cors");
+const compression = require("compression");
+
+//middlewares
+app.use(cors());
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//connection to mongo db
+mongoose
+  .connect(process.env.MONGO_CLOUD_URL)
+  .then(() => {
+    console.log("Mongo DB connected!!");
+  })
+  .catch((error) => {
+    console.log("Mongo DB connection error: ", error);
+  });
+
+//connection to server listening to port 4000
+app.listen(PORT, () => {
+  console.log(`Server connected at http://localhost:${PORT}`);
+});
