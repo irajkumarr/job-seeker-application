99c36d4e6794aeb3e589a938dd109fb3183fd29c
Raj Kumar Timalsina <rajkumar.pc009@gmail.com>
2025-01-17 22:59:40 +0545

    added reference and completed user model

---
+++ backend/models/User/Reference.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const referenceSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    name: { type: String, required: true },
+    designation: { type: String, required: true },
+    organization: { type: String, required: true },
+    email: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const Reference = mongoose.model("Reference", referenceSchema);
+module.exports = Reference;
