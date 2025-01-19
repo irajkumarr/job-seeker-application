00d0a3d9d10135bb4aa3109b0106e8e5634517f9
Raj Kumar Timalsina <rajkumar.pc009@gmail.com>
2025-01-19 22:33:34 +0545

    some changes

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -4,10 +4,9 @@ const storage = multer.memoryStorage();
 const upload = multer({
   storage: storage,
   limits: {
-    fileSize: 5 * 1024 * 1024, // 5MB limit
+    fileSize: 5 * 1024 * 1024,
   },
   fileFilter: (req, file, cb) => {
-    // Allow all file types
     cb(null, true);
   },
 });
