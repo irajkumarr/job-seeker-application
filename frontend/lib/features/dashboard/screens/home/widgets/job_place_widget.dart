56ff65181ff13189f1937d191c6a8e4b6f077015
Raj Kumar Timalsina <rajkumar.pc009@gmail.com>
2025-01-23 23:09:39 +0545

    inkwell widget to all card

--- frontend/lib/features/dashboard/screens/home/widgets/job_place_widget.dart
+++ frontend/lib/features/dashboard/screens/home/widgets/job_place_widget.dart
@@ -15,46 +15,52 @@ class JobPlaceWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 110.w,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(KSizes.sm),
-          border: Border.all(
-            color: KColors.lightContainer,
-          )),
-      margin: EdgeInsets.only(right: KSizes.md - 4),
-      padding: EdgeInsets.all(KSizes.sm),
-      child: Column(
-        children: [
-          SizedBox(
-            width: 50.w,
-            height: 50.h,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(100),
-              child:
-                  // Image.network(
-                  //   "https://www.neptos.io:3000/public/uploads/watermark-thumb/thumb-nimage-1590553913042.jpg",
-                  //   fit: BoxFit.cover,
-                  // ),
-                  CachedNetworkImage(
-                imageUrl:
-                    "https://www.neptos.io:3000/public/uploads/watermark-thumb/thumb-nimage-1590553913042.jpg",
-                placeholder: (context, url) => SizedBox(
-                    width: 50.w,
-                    height: 50.h,
-                    child: Image.asset(KImages.defaultBuilding)),
-                errorWidget: (context, url, error) =>
-                    Image.asset(KImages.defaultBuilding),
-                fit: BoxFit.cover,
+    return Padding(
+      padding: EdgeInsets.only(right: KSizes.md - 4),
+      child: InkWell(
+        onTap: () {},
+        borderRadius: BorderRadius.circular(KSizes.sm),
+        child: Container(
+          width: 110.w,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(KSizes.sm),
+              border: Border.all(
+                color: KColors.lightContainer,
+              )),
+          padding: EdgeInsets.all(KSizes.sm),
+          child: Column(
+            children: [
+              SizedBox(
+                width: 50.w,
+                height: 50.h,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child:
+                      // Image.network(
+                      //   "https://www.neptos.io:3000/public/uploads/watermark-thumb/thumb-nimage-1590553913042.jpg",
+                      //   fit: BoxFit.cover,
+                      // ),
+                      CachedNetworkImage(
+                    imageUrl:
+                        "https://www.neptos.io:3000/public/uploads/watermark-thumb/thumb-nimage-1590553913042.jpg",
+                    placeholder: (context, url) => SizedBox(
+                        width: 50.w,
+                        height: 50.h,
+                        child: Image.asset(KImages.defaultBuilding)),
+                    errorWidget: (context, url, error) =>
+                        Image.asset(KImages.defaultBuilding),
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
-            ),
+              SizedBox(height: KSizes.sm),
+              Text(
+                jobPlace,
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            ],
           ),
-          SizedBox(height: KSizes.sm),
-          Text(
-            jobPlace,
-            style: Theme.of(context).textTheme.titleMedium,
-          ),
-        ],
+        ),
       ),
     );
   }
