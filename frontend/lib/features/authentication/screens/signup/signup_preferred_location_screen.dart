946823d4ae31e66a95595ed087326a886dac7867
Raj Kumar Timalsina <rajkumar.pc009@gmail.com>
2025-01-26 21:07:51 +0545

    starting signup preferred location screen

---
+++ frontend/lib/features/authentication/screens/signup/signup_preferred_location_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SignupPreferredLocationScreen extends StatelessWidget {
+  const SignupPreferredLocationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
