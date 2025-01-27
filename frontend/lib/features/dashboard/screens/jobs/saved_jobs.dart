897e61d5598bfc62104635bca02e298b887c7c75
Raj Kumar Timalsina <rajkumar.pc009@gmail.com>
2025-01-27 23:06:55 +0545

    removed unused variables from dashboard folder

--- frontend/lib/features/dashboard/screens/jobs/saved_jobs.dart
+++ frontend/lib/features/dashboard/screens/jobs/saved_jobs.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/features/dashboard/widgets/login_redirect.dart';
-import 'package:frontend/l10n/l10n.dart';
 import 'package:get_storage/get_storage.dart';
 
 class SavedJobsScreen extends StatelessWidget {
@@ -8,7 +7,6 @@ class SavedJobsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final l10n = AppLocalizations.of(context)!;
     final box = GetStorage();
     final String? token = box.read("token");
     if (token == null) {
--- frontend/lib/features/dashboard/widgets/status_and_saved_jobs_appbar.dart
+++ frontend/lib/features/dashboard/widgets/status_and_saved_jobs_appbar.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:frontend/core/utils/constants/image_strings.dart';
 import 'package:frontend/core/utils/constants/sizes.dart';
-import 'package:frontend/l10n/l10n.dart';
 import 'package:frontend/l10n/language_provider.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:provider/provider.dart';
@@ -15,7 +14,6 @@ class StatusAndSavedJobsAppbar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final l10n = AppLocalizations.of(context)!;
     return Material(
       elevation: 0.2,
       child: AppBar(
